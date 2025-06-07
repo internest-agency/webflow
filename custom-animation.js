1c6740751f42a7fd964f241d02090f76139c87da
Ramkumar R <web@internest.agency>
2025-06-07 12:01:25 +0530

    custom-style-animation

---
+++ custom-animation.js
@@ -0,0 +1,19 @@
+window.addEventListener("scroll", function () {
+  const header = document.querySelector(".menu-container");
+  if (window.scrollY > 50) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+  if (window.scrollY > 300) {
+    goTopBtn.style.display = "block";
+  } else {
+    goTopBtn.style.display = "none";
+  }
+});
+
+const goTopBtn = document.getElementById("goTopBtn");
+
+goTopBtn.addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
